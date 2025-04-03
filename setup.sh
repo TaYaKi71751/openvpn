cda94f19d12ad47285dc87e8c979b843adfa2668
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-03 17:26:58 +0900

    Remove tls-server

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ cat > /etc/openvpn/server.conf << EOF
 port 1194
 proto tcp
 dev tun
-local 172.20.10.1  # Your server's local IP
-ifconfig 10.8.0.1 10.8.0.2  # Static IP assignment
+local 172.20.10.1
+ifconfig 10.8.0.1 10.8.0.2
 keepalive 10 120
 persist-key
 persist-tun
@@ -15,8 +15,7 @@ status /dev/null
 log /dev/null
 verb 3
 
-# Use static key authentication (NO TLS, NO username/password)
-tls-server
+# Use static key authentication (NO TLS, NO DH file)
 secret /etc/openvpn/static.key
 
 # Define a cipher explicitly to avoid warnings
@@ -28,14 +27,13 @@ cat > /var/www/localhost/htdocs/hotspot.ovpn << EOF
 client
 dev tun
 proto tcp
-remote 172.20.10.1 1194  # Your OpenVPN server IP
+remote 172.20.10.1 1194
 resolv-retry infinite
 nobind
 persist-key
 persist-tun
 verb 3
 
-tls-server
 <secret>
 $(cat /etc/openvpn/static.key)
 </secret>
